32b3b4a5ffa42a14a4f76babd5ec00d9580abb14
husseinhassan92 <hussein.hassan2020@outlook.com>
2024-02-03 00:19:02 +0200

    upadte update table

--- Software/update_table.sh
+++ Software/update_table.sh
@@ -44,12 +44,6 @@ case $table_name in
                                             read -p "Enter The Value To Update: " value
                                             if [[ $data_type =~ 'integer' ]]; then
                                                 if [[ $value =~ [0-9]+ ]]; then
-                                                    for ((i=2; i<$index; i++)); do
-                                                        new_row+=":$(sed -n "/^$pk:/p" "$table_name" | cut -d : -f $i)"
-                                                    done
-                                                    
-                                                    echo $data_type
-                                                    new_row+=":$value"
                                                     value_up="$(sed -n "/^$pk:/p" "$table_name" | cut -d : -f $index)"
                                                     sed -i "s/$value_up/$value/" "$table_name"
                                                 else
@@ -76,7 +70,7 @@ case $table_name in
                                         continue
                                     ;;
                                     [Nn])
-                                        break
+                                        break 2
                                     ;;
                                     *)
                                         echo "You Have Entered Invaild Condition"
